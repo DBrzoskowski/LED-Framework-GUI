fa8138e994d0edb116a346d016ba264a3601ce0b
Damian Eggert <s19766@pjwstk.edu.pl>
2022-06-05 12:26:18 +0200

    code refactor, upgrade menu view, add icon, add new background, redesign menu, fixes

--- menu-gui/menu.py
+++ menu-gui/menu.py
@@ -7,13 +7,27 @@ import math
 from vpython import color
 
 
-background = """AppWindow
-{
-background-image: url(background.jpg);
-background-repeat: no-repeat;
-background-position: center;
-height: 843px;
-width: 549px;
+background_style = """AppWindow {
+    background-image: url(background.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 843px;
+    width: 549px;
+}"""
+
+QPushButton_style = """QPushButton {
+    display: block;
+    position: relative;
+    cursor: pointer;
+    background-color: #ffb13d;
+    border-width: 1px;
+    border-color: #ff3045;
+    border-style: solid;
+    border-radius: .8em;
+}
+QPushButton:hover {
+    background-color: #7bff42;
+    transition: background .3s 0.5s;
 }"""
 
 
@@ -22,139 +36,164 @@ class AppWindow(QMainWindow):
         super(AppWindow, self).__init__()
         self.setFixedSize(843, 549)
         self.setWindowTitle("3D LED Framework")
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+
+        # create buttons
+        self.createButton = QtWidgets.QPushButton(self)
+        self.saveFrameButton = QtWidgets.QPushButton(self)
+        self.saveButton = QtWidgets.QPushButton(self)
+        self.endButton = QtWidgets.QPushButton(self)
+        self.openButton = QtWidgets.QPushButton(self)
+        self.loadSpectrumButton = QtWidgets.QPushButton(self)
+        self.colorButton = QtWidgets.QPushButton(self)
+        self.fpsButton = QtWidgets.QPushButton(self)
+        self.startButton = QtWidgets.QPushButton(self)
+        self.stopButton = QtWidgets.QPushButton(self)
+        self.loadButton = QtWidgets.QPushButton(self)
+
+        # create labels
+        self.pathLabel = QtWidgets.QLabel(self)
+        self.colorLabel = QtWidgets.QLabel(self)
+        self.fpsLabel = QtWidgets.QLabel(self)
+        self.animationStateLabel = QtWidgets.QLabel(self)
+        self.readyAnimationLabel = QtWidgets.QLabel(self)
+        self.recentAnimationLabel = QtWidgets.QLabel(self)
+
+        # create boxes
+        self.readyAnimationBox = QtWidgets.QComboBox(self)
+        self.recentAnimationBox = QtWidgets.QComboBox(self)
+
         self.ui()
 
     def ui(self):
-        self.openButton = QtWidgets.QPushButton(self)
-        self.openButton.setGeometry(QtCore.QRect(430, 145, 191, 101))
-        font = QtGui.QFont()
-        font.setPointSize(24)
-        self.openButton.setFont(font)
-        self.openButton.setObjectName("openButton")
-        self.openButton.setStyleSheet("background-color: #55aaff; border-radius: 12px;")
-        self.openButton.clicked.connect(self.showPath)
-
-        self.progressBar = QtWidgets.QProgressBar(self)
-        self.progressBar.setGeometry(QtCore.QRect(20, 460, 411, 23))
-        self.progressBar.setProperty("value", 24)
-        self.progressBar.setObjectName("progressBar")
-
-        self.loadButton = QtWidgets.QPushButton(self)
-        self.loadButton.setGeometry(QtCore.QRect(510, 320, 321, 41))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.loadButton.setFont(font)
-        self.loadButton.setObjectName("loadButton")
-
-        self.readyAnimationBox = QtWidgets.QComboBox(self)
-        self.readyAnimationBox.setGeometry(QtCore.QRect(510, 390, 151, 21))
-        self.readyAnimationBox.setObjectName("readyAnimationBox")
-
-        self.createButton = QtWidgets.QPushButton(self)
-        self.createButton.setGeometry(QtCore.QRect(10, 25, 191, 101))
+        # buttons
+        self.createButton.setGeometry(QtCore.QRect(10, 30, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.createButton.setFont(font)
         self.createButton.setObjectName("createButton")
+        self.createButton.setStyleSheet(QPushButton_style)
         self.createButton.clicked.connect(self.createAnimation)
 
-        self.saveButton = QtWidgets.QPushButton(self)
-        self.saveButton.setGeometry(QtCore.QRect(220, 25, 191, 101))
+        self.saveFrameButton.setGeometry(QtCore.QRect(220, 30, 191, 101))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.saveFrameButton.setFont(font)
+        self.saveFrameButton.setObjectName("saveFrameButton")
+        self.saveFrameButton.setStyleSheet(QPushButton_style)
+
+        self.saveButton.setGeometry(QtCore.QRect(430, 30, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.saveButton.setFont(font)
         self.saveButton.setObjectName("saveButton")
+        self.saveButton.setStyleSheet(QPushButton_style)
 
-        self.endButton = QtWidgets.QPushButton(self)
-        self.endButton.setGeometry(QtCore.QRect(430, 25, 191, 101))
+        self.endButton.setGeometry(QtCore.QRect(640, 30, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.endButton.setFont(font)
         self.endButton.setObjectName("endButton")
+        self.endButton.setStyleSheet(QPushButton_style)
 
-        self.colorButton = QtWidgets.QPushButton(self)
-        self.colorButton.setGeometry(QtCore.QRect(10, 145, 191, 101))
+        self.openButton.setGeometry(QtCore.QRect(10, 150, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(24)
-        font.setBold(False)
+        self.openButton.setFont(font)
+        self.openButton.setObjectName("openButton")
+        self.openButton.setStyleSheet(QPushButton_style)
+        self.openButton.clicked.connect(self.showPath)
+
+        self.loadSpectrumButton.setGeometry(QtCore.QRect(220, 150, 191, 101))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.loadSpectrumButton.setFont(font)
+        self.loadSpectrumButton.setObjectName("loadSpectrumButton")
+        self.loadSpectrumButton.setStyleSheet(QPushButton_style)
+
+        self.colorButton.setGeometry(QtCore.QRect(430, 150, 191, 101))
+        font = QtGui.QFont()
+        font.setPointSize(24)
         font.setWeight(50)
         self.colorButton.setFont(font)
         self.colorButton.setObjectName("colorButton")
+        self.colorButton.setStyleSheet(QPushButton_style)
         self.colorButton.clicked.connect(self.colorPicker)
 
-        self.fpsButton = QtWidgets.QPushButton(self)
-        self.fpsButton.setGeometry(QtCore.QRect(220, 145, 191, 101))
+        self.fpsButton.setGeometry(QtCore.QRect(640, 150, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(24)
         self.fpsButton.setFont(font)
         self.fpsButton.setObjectName("fpsButton")
+        self.fpsButton.setStyleSheet(QPushButton_style)
         self.fpsButton.clicked.connect(self.inputFps)
 
-        self.fpsLabel = QtWidgets.QLabel(self)
-        self.fpsLabel.setGeometry(QtCore.QRect(230, 255, 171, 31))
+        self.startButton.setGeometry(QtCore.QRect(10, 340, 191, 101))
         font = QtGui.QFont()
         font.setPointSize(14)
-        self.fpsLabel.setFont(font)
-        self.fpsLabel.setObjectName("fpsLabel")
+        self.startButton.setFont(font)
+        self.startButton.setStyleSheet(QPushButton_style)
+        self.startButton.setObjectName("startButton")
 
-        self.colorLabel = QtWidgets.QLabel(self)
-        self.colorLabel.setGeometry(QtCore.QRect(20, 255, 171, 31))
+        self.stopButton.setGeometry(QtCore.QRect(220, 340, 191, 101))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.stopButton.setFont(font)
+        self.stopButton.setStyleSheet(QPushButton_style)
+        self.stopButton.setObjectName("stopButton")
+
+        self.loadButton.setGeometry(QtCore.QRect(510, 340, 321, 41))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.loadButton.setFont(font)
+        self.loadButton.setObjectName("loadButton")
+        self.loadButton.setStyleSheet(QPushButton_style)
+
+        # labels
+        self.pathLabel.setGeometry(QtCore.QRect(20, 260, 171, 31))
+        # self.pathLabel.setMaximumSize(QtCore.QSize(350, 65))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.pathLabel.setFont(font)
+        self.pathLabel.setWordWrap(True)
+        self.pathLabel.setObjectName("pathLabel")
+
+        self.colorLabel.setGeometry(QtCore.QRect(440, 260, 171, 31))
         font = QtGui.QFont()
         font.setPointSize(14)
         self.colorLabel.setFont(font)
         self.colorLabel.setObjectName("colorLabel")
 
-        self.pathLabel = QtWidgets.QLabel(self)
-        self.pathLabel.setGeometry(QtCore.QRect(440, 255, 171, 31))
+        self.fpsLabel.setGeometry(QtCore.QRect(650, 260, 171, 31))
         font = QtGui.QFont()
         font.setPointSize(14)
-        self.pathLabel.setFont(font)
-        self.pathLabel.setObjectName("pathLabel")
+        self.fpsLabel.setFont(font)
+        self.fpsLabel.setObjectName("fpsLabel")
 
-        self.stopButton = QtWidgets.QPushButton(self)
-        self.stopButton.setGeometry(QtCore.QRect(220, 320, 191, 101))
+        self.animationStateLabel.setGeometry(QtCore.QRect(30, 460, 361, 41))
         font = QtGui.QFont()
-        font.setPointSize(14)
-        self.stopButton.setFont(font)
-        self.stopButton.setObjectName("stopButton")
+        font.setPointSize(24)
+        self.animationStateLabel.setFont(font)
+        self.animationStateLabel.setObjectName("animationStateLabel")
 
-        self.startButton = QtWidgets.QPushButton(self)
-        self.startButton.setGeometry(QtCore.QRect(10, 320, 191, 101))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.startButton.setFont(font)
-        self.startButton.setObjectName("startButton")
-
-        self.recentAnimationBox = QtWidgets.QComboBox(self)
-        self.recentAnimationBox.setGeometry(QtCore.QRect(680, 390, 151, 21))
-        self.recentAnimationBox.setObjectName("recentAnimationBox")
-        self.readyAnimationLabel = QtWidgets.QLabel(self)
-        self.readyAnimationLabel.setGeometry(QtCore.QRect(510, 370, 151, 21))
+        self.readyAnimationLabel.setGeometry(QtCore.QRect(510, 390, 151, 21))
         font = QtGui.QFont()
         font.setPointSize(8)
         self.readyAnimationLabel.setFont(font)
         self.readyAnimationLabel.setObjectName("readyAnimationLabel")
 
-        self.recentAnimationLabel = QtWidgets.QLabel(self)
-        self.recentAnimationLabel.setGeometry(QtCore.QRect(680, 370, 151, 21))
+        self.recentAnimationLabel.setGeometry(QtCore.QRect(680, 390, 151, 21))
         font = QtGui.QFont()
         font.setPointSize(8)
         self.recentAnimationLabel.setFont(font)
         self.recentAnimationLabel.setObjectName("recentAnimationLabel")
 
-        self.startFrameButton = QtWidgets.QPushButton(self)
-        self.startFrameButton.setGeometry(QtCore.QRect(640, 145, 191, 101))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.startFrameButton.setFont(font)
-        self.startFrameButton.setObjectName("startFrameButton")
+        # boxes
+        self.readyAnimationBox.setGeometry(QtCore.QRect(510, 410, 151, 21))
+        self.readyAnimationBox.setObjectName("readyAnimationBox")
 
-        self.loadSpectrumButton = QtWidgets.QPushButton(self)
-        self.loadSpectrumButton.setGeometry(QtCore.QRect(640, 25, 191, 101))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        self.loadSpectrumButton.setFont(font)
-        self.loadSpectrumButton.setObjectName("loadSpectrumButton")
+        self.recentAnimationBox.setGeometry(QtCore.QRect(680, 410, 151, 21))
+        self.recentAnimationBox.setObjectName("recentAnimationBox")
 
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
@@ -162,22 +201,23 @@ class AppWindow(QMainWindow):
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("AppWindow", "3D LED Framework"))
-        self.openButton.setText(_translate("AppWindow", "Open"))
-        self.loadButton.setText(_translate("AppWindow", "Load selected"))
         self.createButton.setText(_translate("AppWindow", "Create animation"))
+        self.saveFrameButton.setText(_translate("AppWindow", "Save frame"))
         self.saveButton.setText(_translate("AppWindow", "Save animation"))
         self.endButton.setText(_translate("AppWindow", "End animation"))
+        self.openButton.setText(_translate("AppWindow", "Open"))
+        self.pathLabel.setText(_translate("AppWindow", "Path: "))
+        self.loadSpectrumButton.setText(_translate("AppWindow", "Load spectrum"))
         self.colorButton.setText(_translate("AppWindow", "Color"))
+        self.colorLabel.setText(_translate("AppWindow", "Color:"))
         self.fpsButton.setText(_translate("AppWindow", "FPS"))
         self.fpsLabel.setText(_translate("AppWindow", "FPS:"))
-        self.colorLabel.setText(_translate("AppWindow", "Color:"))
-        self.pathLabel.setText(_translate("AppWindow", "Path: "))
-        self.stopButton.setText(_translate("AppWindow", "Stop animation"))
         self.startButton.setText(_translate("AppWindow", "Start animation"))
-        self.readyAnimationLabel.setText(_translate("AppWindow", "3D LED Framework animation"))
+        self.stopButton.setText(_translate("AppWindow", "Stop animation"))
+        self.animationStateLabel.setText(_translate("AppWindow", "Animation state: Inactive"))
+        self.loadButton.setText(_translate("AppWindow", "Load selected"))
+        self.readyAnimationLabel.setText(_translate("AppWindow", "3D LED Framework animations"))
         self.recentAnimationLabel.setText(_translate("AppWindow", "Recent animations"))
-        self.startFrameButton.setText(_translate("AppWindow", "Start frame"))
-        self.loadSpectrumButton.setText(_translate("AppWindow", "Load Spectrum"))
 
     def colorPicker(self):
         # opening color dialog
@@ -190,6 +230,8 @@ class AppWindow(QMainWindow):
             self.colorLabel.adjustSize()
         else:
             self.colorLabel.setText("Color: ")
+        # return color_name
+        # Cube3D.drawing(self, color_name)
 
     def showPath(self):
         # open file dialog
@@ -198,22 +240,33 @@ class AppWindow(QMainWindow):
         if file:
             self.pathLabel.setText("Path: " + file[0])
             self.pathLabel.setWordWrap(True)
+            # font = QtGui.QFont()
+            # font.setPointSize(8)
+            # self.pathLabel.setFont(font)
             self.pathLabel.adjustSize()
 
     def inputFps(self):
         fps_input = QInputDialog.getInt(self, "Input fps", "FPS: ")
         if input:
-            self.fpsLabel.setText("FPS: " + str(fps_input[0]))
-            self.pathLabel.adjustSize()
+            if 10 <= int(fps_input[0]) <= 60:
+                self.fpsLabel.setText("FPS: " + str(fps_input[0]))
+                self.pathLabel.adjustSize()
+            else:
+                msg = QMessageBox()
+                msg.setWindowTitle("Error")
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText("FPS value should be between 10 to 60.")
+                msg.exec_()
 
     def createAnimation(self):
         c = Cube3D(8, 0.15 * 1, 1, 0.1 * 1 * math.sqrt(1 / 1))
         c.background = color.white
+        # c.drawing()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyleSheet(background)
+    app.setStyleSheet(background_style)
     win = AppWindow()
     win.show()
     sys.exit(app.exec_())
