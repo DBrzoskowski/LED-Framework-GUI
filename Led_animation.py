1f5d9dd6f74eaa4e68baa277050ce3ffef607f63
Damian Brzoskowski (s18499) <s18499@pjwstk.edu.pl>
2022-08-04 19:45:12 +0200

    Add changes after merge with newest version

--- Led_animation.py
+++ Led_animation.py
@@ -6,19 +6,16 @@ Installation:
     pip install vpython
 """
 import time
-from random import uniform
+import json
 import txaio
-from vpython import canvas, scene, vector, sqrt, sphere, vec, color, curve, sleep, distant_light
+from random import uniform
+from colormap import hex2rgb
+from vpython import canvas, scene, vector, sphere, vec, color, curve, sleep, distant_light, button, rate
 import asyncio
 # scene.background = vector(0.95, 1, 1) # white background
 txaio.use_asyncio()  # resolve problem with library https://stackoverflow.com/questions/34157314/autobahn-websocket-issue-while-running-with-twistd-using-tac-file
-scene.background = color.white  # scene color
-N = 8  # N by N by N array of leds
-k = 1
-m = 1
-spacing = 1
-led_radius = 0.15 * spacing
-old_led_color = {}
+
+ANIMATION_FILE = 'animation_path.txt'
 
 
 def fps_to_milliseconds(fps):
@@ -29,7 +26,7 @@ class Cube3D(canvas):
     def __init__(self, size, led_radius, spacing, momentumRange, **args):
         super().__init__(**args)
         self.leds = []
-        self.center = 0.5 * (N - 1) * vector(1, 1, 1)  # camera start view
+        self.center = 0.5 * (8 - 1) * vector(1, 1, 1)  # camera start view
         self.caption = """A model of a solid represented as leds connected by interledic bonds.
 
         To rotate "camera", drag with right button or Ctrl-drag.
@@ -38,32 +35,24 @@ class Cube3D(canvas):
         To pan left/right and up/down, Shift-drag.
         Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
         self.lights = []
+        self.old_led_color = {}
 
-        # self.old_led_color = {}
+        # The part responsible for drawing
+        self.drawing_path = {}
+        self.drawing_path.setdefault('pos', [])
+        self.drawing_path.setdefault('color', [])
+        self.drawing_path.setdefault('fps', 30)
+        self.drawing_color = None
+        self.drawing_fps = None
+        self.drawing_button_status = False
+        self.animation_frame = []
+        self.animation_step = []
+        self.button_drawing = button(text="Not drawing", pos=self.title_anchor, bind=self.drawing_status)
 
-        # TODO: WIP work on light for each wall
         distant_light(direction=vector(0.22,  0.44,  0.88), color=color.gray(0.8))
         distant_light(direction=vector(-0.88, -0.22, -0.44), color=color.gray(0.3))
         distant_light(direction=vector(65.22,  65.44,  65.88), color=color.gray(0.8))
         distant_light(direction=vector(-65.88, -65.22, -65.44), color=color.gray(0.3))
-
-        # Create (N+2)^3 LED in a grid; the outermost LED is fixed and invisible
-        # for z in range(-1, size + 1, 1):
-        #     for y in range(-1, size + 1, 1):
-        #         for x in range(-1, size + 1, 1):
-        #             led = sphere()
-        #             led.pos = vector(x, y, z) * spacing
-        #             led.radius = led_radius
-        #             led.color = vector(0, 0.58, 0.69)
-        #             if 0 <= x < size and 0 <= y < size and 0 <= z < size:
-        #                 p = vec.random()
-        #                 led.momentum = momentumRange * p
-        #                 led.color = color.black
-        #             else:
-        #                 led.visible = False
-        #                 led.momentum = vec(0, 0, 0)
-        #             led.index = len(self.leds)
-        #             self.leds.append(led)
 
         for z in range(0, size, 1):
             for x in range(0, size, 1):
@@ -80,46 +69,22 @@ class Cube3D(canvas):
                         led.momentum = vec(0, 0, 0)
                     led.index = len(self.leds)
                     self.leds.append(led)
-        # Create lines between LED
-        for led in self.leds:
-            if led.visible:
-                pass
-                """
-                if led.pos.x == 0:
-                    self.make_line(self.leds[led.index - 1], led)
-                    self.make_line(led, self.leds[led.index + 1])
-                elif led.pos.x == size - 1:
-                    self.make_line(led, self.leds[led.index + 1])
-                else:
-                    self.make_line(led, self.leds[led.index + 1])
 
-                if led.pos.y == 0:
-                    self.make_line(self.leds[led.index - (size + 2)], led)
-                    self.make_line(led, self.leds[led.index + (size + 2)])
-                elif led.pos.y == size - 1:
-                    self.make_line(led, self.leds[led.index + (size + 2)])
-                else:
-                    self.make_line(led, self.leds[led.index + (size + 2)])
+    @staticmethod
+    def hex2vector(drawing_color):
+        try:
+            if isinstance(drawing_color, str) and drawing_color.startswith('#'):
+                r, g, b = hex2rgb(drawing_color)
+                r, g, b = r / 255, g / 255, b / 255
+                return vector(r, g, b)
+        except TypeError as err:
+            print(f"Wrong type {err}")
 
-                if led.pos.z == 0:
-                    self.make_line(self.leds[led.index - (size + 2) ** 2], led)
-                    self.make_line(led, self.leds[led.index + (size + 2) ** 2])
-                elif led.pos.z == size - 1:
-                    self.make_line(led, self.leds[led.index + (size + 2) ** 2])
-                else:
-                    self.make_line(led, self.leds[led.index + (size + 2) ** 2])
-                """
-    # Create line between LEDs
-
-    def visible_leds(self):
+    def get_visible_leds(self):
         return [i for i in self.leds if i.visible is True]
 
-    def make_line(self, start, end):
-        test_list = [vector(start.pos), vector(end.pos)]
-        return curve(pos=test_list)
-
     def change_color(self, v):
-        leds = self.visible_leds()
+        leds = self.get_visible_leds()
         if isinstance(v, str):
             color_picker = {
                 'black': vector(0, 0, 0),
@@ -143,90 +108,82 @@ class Cube3D(canvas):
         for i in leds:
             i.color = cp
 
-    def random_color(self):
-        leds = self.visible_leds()  # 512 visible LEDs
-        for i in leds:
-            i.color = vector(uniform(0, 1), uniform(0, 1), uniform(0, 1))
-
     def get_led_from_visible(self, position):
         return [i for i in self.leds if (i.pos.z, i.pos.y, i.pos.x) == position][0]
 
-    def test_whole_layer(self):
+    def reset_cube_state(self):
+        for led in self.leds:
+            led.color = vector(0, 0, 0)
+        sleep(0.5)
+
+    def random_color_animation(self):
+        leds = self.get_visible_leds()  # 512 visible LEDs
+        for i in leds:
+            i.color = vector(uniform(0, 1), uniform(0, 1), uniform(0, 1))
+
+    def outer_layer_animation(self, col=vector(1, 1, 1), fps=30):
+        if self.drawing_color and self.drawing_fps:
+            col = self.drawing_color
+            fps = self.drawing_fps
+
         for y in range(0, 8):
             get_all = [self.get_led_from_visible((0, y, i)) for i in range(0, 8)]
             for i in get_all:
-                i.color = vector(1, 1, 1)
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in range(0, 8):
             get_all = [self.get_led_from_visible((y, 7, i)) for i in range(0, 8)]
             for i in get_all:
-                i.color = vector(1, 1, 1)
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in reversed(range(0, 8)):
             get_all = [self.get_led_from_visible((7, y, i)) for i in range(0, 8)]
             for i in get_all:
-                i.color = vector(1, 1, 1)
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in reversed(range(0, 8)):
             get_all = [self.get_led_from_visible((y, 0, i)) for i in range(0, 8)]
             for i in get_all:
-                i.color = vector(1, 1, 1)
-            sleep(0.2)
+                i.color = col
+            rate(fps)
 
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+    def outline_inside_ankle_animation(self, col=vector(1, 0, 0), fps=30):
+        if self.drawing_color and self.drawing_fps:
+            col = self.drawing_color
+            fps = self.drawing_fps
 
-    def test_whole_inside_layer(self, color):
         for y in range(2, 6):
             get_all = [self.get_led_from_visible((2, y, i)) for i in range(2, 6)]
             for i in get_all:
-                i.color = color
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in range(2, 6):
-            get_all = [self.get_led_from_visible((y, 6, i)) for i in range(2, 6)]
+            get_all = [self.get_led_from_visible((y, 5, i)) for i in range(2, 6)]
             for i in get_all:
-                i.color = color
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in reversed(range(2, 6)):
-            get_all = [self.get_led_from_visible((6, y, i)) for i in range(2, 6)]
+            get_all = [self.get_led_from_visible((5, y, i)) for i in range(2, 6)]
             for i in get_all:
-                i.color = color
-            sleep(0.2)
-
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+                i.color = col
+            rate(fps)
 
         for y in reversed(range(2, 6)):
             get_all = [self.get_led_from_visible((y, 2, i)) for i in range(2, 6)]
             for i in get_all:
-                i.color = color
-            sleep(0.2)
+                i.color = col
+            rate(fps)
 
-            for i in get_all:
-                i.color = vector(0, 0, 0)
+    def double_outline_animation(self, col=vector(1, 0, 0), fps=30):
+        if self.drawing_color and self.drawing_fps:
+            col = self.drawing_color
+            fps = self.drawing_fps
 
-    def comibne_2_tests(self, color):
-        # TODO: 1. This need to be re-write
         for y in range(0, 8):
             get_all_1 = [self.get_led_from_visible((0, y, i)) for i in range(0, 8)]
             get_all_2 = [self.get_led_from_visible((2, y, i)) for i in range(2, 6) if y in [2, 3, 4, 5]]
@@ -235,117 +192,121 @@ class Cube3D(canvas):
                 i.color = vector(1, 1, 1)
 
             for i in get_all_2:
-                i.color = color
+                i.color = col
 
-            sleep(0.2)
-
-            # clear row after color showed
-            # for i in get_all_1:
-            #     i.color = vector(0, 0, 0)
-            #
-            # for i in get_all_2:
-            #     i.color = vector(0, 0, 0)
+            rate(fps)
 
         for y in range(0, 8):
             get_all_1 = [self.get_led_from_visible((y, 7, i)) for i in range(0, 8)]
-            get_all_2 = [self.get_led_from_visible((y, 5, i)) for i in range(2, 6) if y in [3, 4, 5, 6]]
-
+            get_all_2 = [self.get_led_from_visible((y, 5, i)) for i in range(2, 6) if y in [3, 4, 5]]
             for i in get_all_1:
                 i.color = vector(1, 1, 1)
 
             for i in get_all_2:
-                i.color = color
+                i.color = col
 
-            sleep(0.2)
-
-            # for i in get_all_1:
-            #     i.color = vector(0, 0, 0)
-            #
-            # for i in get_all_2:
-            #     i.color = vector(0, 0, 0)
+            rate(fps)
 
         for y in reversed(range(0, 8)):
             get_all_1 = [self.get_led_from_visible((7, y, i)) for i in range(0, 8)]
-            get_all_2 = [self.get_led_from_visible((6, y, i)) for i in range(2, 6) if y in [3, 4, 5]]
-
+            get_all_2 = [self.get_led_from_visible((5, y, i)) for i in range(2, 6) if y in [3, 4, 5]]
             for i in get_all_1:
                 i.color = vector(1, 1, 1)
 
             for i in get_all_2:
-                i.color = color
+                i.color = col
 
-            sleep(0.2)
-
-            # for i in get_all_1:
-            #     i.color = vector(0, 0, 0)
-            #
-            # for i in get_all_2:
-            #     i.color = vector(0, 0, 0)
+            rate(fps)
 
         for y in reversed(range(0, 8)):
-            get_all_1 = [self.get_led_from_visible((y, 0, i)) for i in range(0, 8)] 
-            get_all_2 = [self.get_led_from_visible((y, 2, i)) for i in range(2, 6) if y in [3, 4, 5, 6]]
-
+            get_all_1 = [self.get_led_from_visible((y, 0, i)) for i in range(0, 8)]
+            get_all_2 = [self.get_led_from_visible((y, 2, i)) for i in range(2, 6) if y in [3, 4, 5]]
             for i in get_all_1:
                 i.color = vector(1, 1, 1)
 
             for i in get_all_2:
-                i.color = color
+                i.color = col
 
-            sleep(0.2)
+            rate(fps)
 
-            # for i in get_all_1:
-            #     i.color = vector(0, 0, 0)
-            #
-            # for i in get_all_2:
-            #     i.color = vector(0, 0, 0)
+    def save_animation_to_frame(self, file_path=ANIMATION_FILE, to_file=False):
+        if self.drawing_path['pos'] and self.drawing_path['color']:
+            return self.drawing_path
 
-    def layers_change(self):
-        # WIP function
-        leds = self.visible_leds()
-        leds[0].color = color.orange
-        leds[1].color = color.white
-        # leds[2].color = color.red
-        # leds[3].color = color.green
-        # leds[4].color = color.blue
-        # leds[5].color = color.yellow
-        # leds[6].color = color.cyan
-        # leds[7].color = color.magenta
-        # leds[8].color = color.orange
+    def load_animation_from_file(self, file_path=ANIMATION_FILE):
+        with open(file_path, 'r') as f:
+            for i in f.readlines():
+                line = json.loads(i)
 
-        leds[503].color = color.blue
-        leds[504].color = color.orange
-        leds[505].color = color.red
-        leds[506].color = color.green
-        leds[511].color = color.yellow
+                if line.get('color'):
+                    colors = line.get('color')
 
-        leds[448].color = color.yellow
+                if line.get('pos'):
+                    for pos in line.get('pos'):
+                        # this must be reverse because we start drawing from the z axis
+                        # e.g | x, y, z -> (6.0, 0.0, 7.0) | ==> | x, y, z -> (7.0, 0.0, 6.0) |
+                        pos.reverse()
+                        self.animation_step.append(self.get_led_from_visible(tuple(pos)))
 
-    def drawing(self, drawing_color=color.red, default_color=color.black):
+                if line.get('fps'):
+                    fps = line.get('fps')
+
+                # animation process
+                for led, col in zip(self.animation_step, colors):
+                    r, g, b = col
+                    led.color = vector(r, g, b)
+
+                # clear animation step list
+                self.animation_step = []
+
+                # fps after chunk of animation end and waiting for next part
+                rate(fps)
+        # it's sleep because rate working based on windows time
+        # (resolve problem with unexpected speed-up animation)
+        sleep(fps_to_milliseconds(fps))
+
+    def gui_args_builder(self, drawing_color, fps):
+        self.drawing_color = drawing_color
+        self.drawing_fps = fps
+        try:
+            if self.drawing_color and self.drawing_fps:
+                return self.drawing_color, self.drawing_fps
+        except ReferenceError as err:
+            print(f"One of the arguments hasn't been defined -> {err}")
+
+    def drawing(self, drawing_color=color.red, default_color=color.black, fps=30):
+        # Get info from GUI about color and fps
+        if self.drawing_color and self.drawing_fps:
+            drawing_color = self.drawing_color
+            fps = self.drawing_fps
+
         self.waitfor('click')
         hit = self.mouse.pick
+        self.drawing_path['fps'] = fps
+
+        drawing_color = self.hex2vector(drawing_color)
 
         if hit:
             if hit.color != drawing_color:
-                old_led_color[hit.idx] = default_color
+                self.old_led_color[hit.idx] = default_color
 
-            hit.color = drawing_color if hit.color == old_led_color[hit.idx] else old_led_color[hit.idx]
+            hit.color = drawing_color if hit.color == self.old_led_color[hit.idx] else self.old_led_color[hit.idx]
 
+            self.drawing_path['pos'].append(hit.pos.value)
+            self.drawing_path['color'].append(hit.color.value)
 
-c = Cube3D(N, led_radius, spacing, 0.1 * spacing * sqrt(k / m))
-c.background = color.white  # temporarily to see the leds better
-# time.sleep(2)
-# c.comibne_2_tests(color=vector(1, 0, 0))
-# While it's unnecessary
-while True:
-    drawing = True
+    def change_drawing_status_from_gui(self, value):
+        self.drawing_button_status = value
+        if value:
+            self.button_drawing.delete()
+            self.button_drawing = button(text="Drawing", pos=self.title_anchor, bind=self.drawing_status)
+        else:
+            self.button_drawing.delete()
+            self.button_drawing = button(text="Not drawing", pos=self.title_anchor, bind=self.drawing_status)
 
-    if not drawing:
-        c.comibne_2_tests(color=vector(1, 0, 0))
-
-        # clear whole cube after test finish
-        for i in c.visible_leds():
-            i.color = vector(0, 0, 0)
-        sleep(0.2)
-    else:
-        c.drawing()
+    def drawing_status(self, b):
+        self.drawing_button_status = not self.drawing_button_status
+        if self.drawing_button_status:
+            b.text = "Drawing"
+        else:
+            b.text = "Not drawing"
